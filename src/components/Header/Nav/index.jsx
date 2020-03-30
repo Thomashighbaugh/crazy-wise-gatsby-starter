94e32ef8811b5547700c1848d830d83c05aee294
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-03-30 16:53:41 -0700

    git nightmares averted

--- src/components/Header/Nav/index.jsx
+++ src/components/Header/Nav/index.jsx
@@ -2,21 +2,6 @@ import React, { Component } from "react"
 import Link from "gatsby-link"
 //@TODO add in nav links for each page
 const Nav = () => (
-<<<<<<< HEAD
-  <nav className="navbar">
-    <div className="btn-group links">
-      <Link className="nav-link btn" id="nav0" to="/">
-        HOME
-      </Link>
-
-      <Link to="/about/" onlyActiveOnIndex className="btn nav-link " id="nav1">
-        ABOUT
-      </Link>
-      <Link
-        to="/projects/"
-        onlyActiveOnIndex
-        className="btn nav-link "
-=======
   <nav className="navbar fixed-top navbar-expand-lg ">
       <ul className="navbar-nav">
           <li className="nav-item mr-1">
@@ -35,25 +20,10 @@ const Nav = () => (
         to="/page-3/"
         onlyActiveOnIndex
         className=" nav-link "
->>>>>>> 5635ce8... almost there
         id="nav2"
       >
         ARCHIVE
       </Link>
-<<<<<<< HEAD
-      <Link
-        to="/projects/"
-        onlyActiveOnIndex
-        className="btn nav-link "
-        id="nav3"
-      >
-        TEST{" "}
-      </Link>
-      <Link
-        to="/projects/"
-        onlyActiveOnIndex
-        className="btn nav-link "
-=======
           </li>
           <li className="nav-item mr-1">
           <Link
@@ -71,17 +41,12 @@ const Nav = () => (
         to="/page-5/"
         onlyActiveOnIndex
         className=" nav-link "
->>>>>>> 5635ce8... almost there
         id="nav4"
       >
         TEST
       </Link>
-<<<<<<< HEAD
-    </div>
-=======
           </li>
       </ul>
->>>>>>> 5635ce8... almost there
   </nav>
 )
 
