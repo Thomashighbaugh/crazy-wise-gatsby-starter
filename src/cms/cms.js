12a259e0336af75d3cdf8fd1c4611739170d550f
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-04-08 16:24:11 -0700

    this site is driving me nuts

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,10 @@ import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import AboutPagePreview from "./preview-templates/AboutPagePreview";
-import BlogPostPreview from "./preview-templates/BlogPostPreview";
-import ProductPagePreview from "./preview-templates/ProductPagePreview";
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
+// import AboutPagePreview from "/src/cms/preview-templates/AboutPagePreview";
+// import BlogPostPreview from "/src/cms/preview-templates/BlogPostPreview";
+// import ProductPagePreview from "/src/cms/preview-templates/ProductPagePreview";
+// import IndexPagePreview from "/src/cms/preview-templates/IndexPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
